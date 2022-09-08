a92d78565ef85fc0ab78fbc524f8e763ebd2e829
smirnakh <99379073+smirnakh@users.noreply.github.com>
2022-09-08 10:13:54 -0700

    completed

--- api/server.js
+++ api/server.js
@@ -5,15 +5,48 @@ const server = express();
 
 server.use(express.json());
 
-server.delete('/api/users/:id', async (req, res) => {
-  const possibleUser = await User.findById(req.params.id);
-  if (!possibleUser) {
-    res.status(404).json({
-      message: 'The user with the specified ID does not exist',
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist',
+      });
+    }
+    if (!req.body.name || !req.body.bio) {
+      res.status(400).json({
+        message: 'Please provide name and bio for the user',
+      });
+    } else {
+      const updatedUser = await User.update(req.params.id, req.body);
+      res.status(200).json(updatedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The user information could not be modified',
+      err: err.message,
+      stack: err.stack,
     });
-  } else {
-    const deletedUser = await User.remove(possibleUser.id);
-    res.status(200).json(deletedUser);
+  }
+});
+
+server.delete('/api/users/:id', async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist',
+      });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error deleting user',
+      err: err.message,
+      stack: err.stack,
+    });
   }
 });
 
